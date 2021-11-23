771e5739f242c7287950a6c4557aaf732e76078a
rifqikhanif <94571775+rifqikhanif@users.noreply.github.com>
2021-11-23 19:24:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,5 @@ apt-get install wget
 apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm start
+git clone https://github.com/rifqikhanif/rifqi-bot && cd rifqi-bot && bash install.sh
 
